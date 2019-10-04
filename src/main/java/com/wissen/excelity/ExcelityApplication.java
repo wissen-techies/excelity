e96d0fb971f96ded0afcc80f19043879df6af779
amar <amarjsaikia@gmail.com>
2019-10-04 08:52:07 +0530

    First Commit

---
+++ src/main/java/com/wissen/excelity/ExcelityApplication.java
@@ -0,0 +1,13 @@
+package com.wissen.excelity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelityApplication.class, args);
+	}
+
+}
